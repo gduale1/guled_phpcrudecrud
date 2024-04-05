fdaa86d81e9328c1e53aca7ae6acef7cb5f3b2ed
gduale1 <guledduale00@gmail.com>
2024-04-05 06:39:30 +0000

    commit on lab 7

---
+++ dynamic.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Dynamic Web Page</title>
+</head>
+<body>
+<h2>This is a Heading</h2>
+<p>This is a paragraph</p>
+
+<!--This is an HTML Comment. It will not be displayed-->
+
+<!--Print a date using server-side PHP.-->
+<?php
+echo date(DATE_RFC2822);
+?>
+
+<br>
+<br>
+
+<!--Print a date using client-side JavaScript.-->
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time using JavaScript.</button>
+<br>
+<a href="http://192.168.17.133/">HOME</a>
+<p id="demo"></p>
+
+</body>
+</html>
